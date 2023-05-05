edcd70dae38db5112a28e0cd9aa5799dfb63662a
zoesacks <102333575+zoesacks@users.noreply.github.com>
2023-05-05 19:16:08 -0300

    2

--- LostGalaxian/src/juego/ProyectilNave.java
+++ LostGalaxian/src/juego/ProyectilNave.java
@@ -1,5 +1,28 @@
 package juego;
 
-public class ProyectilNave {
+import java.awt.Image;
 
+import entorno.Entorno;
+import entorno.Herramientas;
+
+public class ProyectilNave {
+	double x;
+	double y;
+	double angulo;
+	Image img;
+	
+	public ProyectilNave(int x, int y) {
+		
+		this.x = x;
+		this.y = y;
+		this.img = Herramientas.cargarImagen("proyectil-nave.png");
+		this.angulo = 0;
+
+	}
+	
+	public void dibujarse(Entorno entorno) {
+	
+		entorno.dibujarImagen(img, x, y, angulo);;
+	
+	}
 }
